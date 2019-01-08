241340ebe9f8f8c7d938547027e6b09d79177062
GeniusTang <710783447@qq.com>
2019-01-08 15:25:24 -0800

    correct arguments being passed as 0 by PyArg_ParseTuple

--- src/_count.cpp
+++ src/_count.cpp
@@ -9,12 +9,18 @@
 static PyObject *kmer_count(PyObject *self, PyObject *args)
 {
     char* filename;
-    int NumThreads=1;
+    int x, K, NumThreads;
     bool Reverse=false;
-    int K=0;
-    if (!PyArg_ParseTuple(args, "siii", &filename, &K, &NumThreads, &Reverse))
+    if (!PyArg_ParseTuple(args, "siip", &filename, &x, &NumThreads, &Reverse))
+        return NULL;
+    if (!PyArg_ParseTuple(args, "siip", &filename, &K, &NumThreads, &Reverse))
         return NULL;
     npy_intp SIZE = pow(4, K);
+    /*
+    std::cout << "K: " << K << std::endl;
+    std::cout << "Threads: " << NumThreads << std::endl;
+    std::cout << "Reverse: " << Reverse << std::endl;
+    */
     static std::vector<std::atomic<int>> count_array;
     count_array = count(filename, K, NumThreads, Reverse);
     return PyArray_SimpleNewFromData(1, &SIZE, NPY_INT32, static_cast<void*>(count_array.data()));
@@ -23,11 +29,17 @@ static PyObject *kmer_count(PyObject *self, PyObject *args)
 static PyObject *kmer_count_seq(PyObject *self, PyObject *args)
 {
     char* sequence;
-    int K=0;
-    int NumThreads=1;
+    int x , K, NumThreads;
     bool Reverse=false;
-    if (!PyArg_ParseTuple(args, "siii", &sequence, &K, &NumThreads, &Reverse))
+    if (!PyArg_ParseTuple(args, "siip", &sequence, &x, &NumThreads, &Reverse))
         return NULL;
+    if (!PyArg_ParseTuple(args, "siip", &sequence, &K, &NumThreads, &Reverse))
+        return NULL;
+    /*
+    std::cout << "K: " << K << std::endl;
+    std::cout << "Threads: " << NumThreads << std::endl;
+    std::cout << "Reverse: " << Reverse << std::endl;
+    */
     npy_intp SIZE = pow(4, K);
     static std::vector<std::atomic<int>> count_array;
     count_array = count_seq(sequence, K, NumThreads, Reverse);
@@ -37,12 +49,18 @@ static PyObject *kmer_count_seq(PyObject *self, PyObject *args)
 static PyObject *kmer_count_m_k(PyObject *self, PyObject *args)
 {
     char* filename;
-    int K=0;
-    int M=0;
-    int NumThreads=1;
-    bool Reverse=false;
-    if (!PyArg_ParseTuple(args, "siiii", &filename, &M, &K, &NumThreads, &Reverse))
+    int x, M, K, NumThreads;
+    bool Reverse;
+    if (!PyArg_ParseTuple(args, "siiip", &filename, &x, &K, &NumThreads, &Reverse))
         return NULL;
+    if (!PyArg_ParseTuple(args, "siiip", &filename, &M, &K, &NumThreads, &Reverse))
+        return NULL;
+    /*
+    std::cout << "M: " << M << std::endl;
+    std::cout << "K: " << K << std::endl;
+    std::cout << "Threads: " << NumThreads << std::endl;
+    std::cout << "Reverse: " << Reverse << std::endl;
+    */
     npy_intp SIZE = pow(4, K) + pow(4, M); 
     static std::vector<std::atomic<int>> count_array;
     count_array = count_M_K(filename, M, K, NumThreads, Reverse);
@@ -52,12 +70,18 @@ static PyObject *kmer_count_m_k(PyObject *self, PyObject *args)
 static PyObject *kmer_count_m_k_seq(PyObject *self, PyObject *args)
 {
     char* sequence;
-    int K=0;
-    int M=0;
-    int NumThreads=1;
-    bool Reverse=false;
-    if (!PyArg_ParseTuple(args, "siiii", &sequence, &M, &K, &NumThreads, &Reverse))
+    int x, M, K, NumThreads;
+    bool Reverse;
+    if (!PyArg_ParseTuple(args, "siiip", &sequence, &x, &K, &NumThreads, &Reverse))
         return NULL;
+    if (!PyArg_ParseTuple(args, "siiip", &sequence, &M, &K, &NumThreads, &Reverse))
+        return NULL;
+    /*
+    std::cout << "M: " << M << std::endl;
+    std::cout << "K: " << K << std::endl;
+    std::cout << "Threads: " << NumThreads << std::endl;
+    std::cout << "Reverse: " << Reverse << std::endl;
+    */
     npy_intp SIZE = pow(4, K) + pow(4, M);
     static std::vector<std::atomic<int>> count_array;
     count_array = count_M_K_seq(sequence, M, K, NumThreads, Reverse);
